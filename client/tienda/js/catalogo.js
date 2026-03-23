5e6efccff7a1fc6625173f5444e7d0f364cfba58
desco10 <daniel1041esk.arb@gmail.com>
2026-03-22 21:45:22 -0500

    fix catalogo fetch produccion

--- client/tienda/js/catalogo.js
+++ client/tienda/js/catalogo.js
@@ -17,8 +17,7 @@ async function cargarProductos(){
 
   try{
 
-    const res = await fetch("http://localhost:3000/productos");
-
+  const res = await fetch("https://revistalogo-backend.onrender.com/productos");
     const data = await res.json();
 
     productosGlobal = data;
