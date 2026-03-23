8e92909731171446a960439ea349de9fb7093646
desco10 <daniel1041esk.arb@gmail.com>
2026-03-22 20:25:02 -0500

    conexion correcta railway

--- server/config/db.js
+++ server/config/db.js
@@ -1,18 +1,12 @@
 const mysql = require("mysql2/promise");
-require("dotenv").config();
 
 const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
+  uri: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
-  }
+  },
+  waitForConnections: true,
+  connectionLimit: 10
 });
 
 // prueba de conexión
@@ -22,7 +16,7 @@ const db = mysql.createPool({
     console.log("Conectado a MySQL 🚀");
     connection.release();
   } catch (error) {
-    console.error("Error conectando a MySQL:", error);
+    console.error("Error conectando a MySQL:", error.message);
   }
 })();
 
